415423b4b2287df5414ded08ac489b792a51797d
Mateusz Pyzik <mateusz.pyzik@outlook.com>
2025-10-02 14:36:40 +0200

    Initial commit

---
+++ default.nix
@@ -0,0 +1,29 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
+stdenv.mkDerivation {
+  name = "T-Axi";
+  buildInputs = [ (texlive.combine {
+                    inherit (texlive)
+                      scheme-small
+
+                      # Add other LaTeX libraries (packages) here as needed, e.g:
+                      # xparse
+                      stmaryrd
+                      mathpartir
+
+                      # build tools
+                      latexmk
+                      ;
+                  })
+                  glibcLocales
+                ];
+  src = ./.;
+
+  meta = with lib; {
+    description = "Prototype of type theoretical Axi - docs and implementation";
+    license = licenses.isc;
+    platforms = platforms.linux;
+  };
+}
